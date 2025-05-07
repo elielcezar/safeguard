ffce8f5f0ab733dcf2e6a645ca3e990b02b2e444
Eliel Cezar <elielcezar@gmail.com>
2025-05-07 00:59:43 -0300

    add rotina de exclusão de senha

--- back/routes/protected.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// Rota protegida de exemplo
-router.get("/test", (req, res) => {
-  res.json({ message: "Rota protegida funcionando" });
-});
-
-export default router; 
