1bdfb0cc9a064492a3f9ab0c8c6ab62e19f91dab
Eliel Cezar <elielcezar@gmail.com>
2025-05-09 14:08:25 -0300

    pagina de edição do usuario - pt 1

--- front/src/pages/User/Profile/index.jsx
+++ front/src/pages/User/Profile/index.jsx
@@ -1,47 +1,144 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { toast, useToast } from "@/components/ui/use-toast";
+import api from '@/services/api';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+
 
 export default function Profile() {
+  
+  const navigate = useNavigate();
+  const { dismiss } = useToast();
+  const [loading, setLoading] = useState(false);
 
-  const [userData, setUserData] = useState({});
-
-    // Busca os dados do usuário do localStorage
+  const [name, setName] = useState('');  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');    
+  
+  // Busca os dados do usuário do localStorage
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user) {
-      setUserData(user);
-    }    
-    // Buscar a lista de senhas
-    fetchPasswords();
+    const userJson = localStorage.getItem('user');
+    if (userJson) {
+      try {
+        const user = JSON.parse(userJson);        
+        setName(user.name || '');
+        setEmail(user.email || '');
+        setPassword(user.password || '');
+      } catch (error) {
+        console.error('Erro ao parsear dados do usuário:', error);
+      }
+    }        
   }, []);
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    console.log(name, email, password);
+
+    try {
+      await api.put('/api/users', { name, email, password });
+      toast({
+        title: 'Usuário atualizado com sucesso',
+        description: 'O usuário foi atualizado com sucesso',
+      });
+      setTimeout(() => {
+        navigate('/');
+        dismiss();  
+      }, 2000);
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: 'Erro ao atualizar usuário',
+        description: 'Ocorreu um erro ao atualizar o usuário',
+      });
+      console.error('Erro ao atualizar usuário:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
-      <Card className="mb-6">
-                <CardHeader>
-                  <CardDescription>
-                    Informações da sua conta
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <div className="space-y-4">
-                    <div className="flex flex-col space-y-1">
-                      <span className="text-sm text-gray-500">Nome</span>
-                      <span className="font-medium text-lg">{userData.name}</span>
-                    </div>
-                    <div className="flex flex-col space-y-1">
-                      <span className="text-sm text-gray-500">Email</span>
-                      <span className="font-medium">{userData.email}</span>
-                    </div>
-                    <div className="flex flex-col space-y-1">
-                      <span className="text-sm text-gray-500">Função</span>
-                      <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
-                        {userData.role}
-                      </span>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
+    <div className="min-h-screen">
+            <SidebarProvider>
+
+                <AppSidebar variant="inset"/>
+
+                <main className="flex w-full flex-1 gap-5 p-6 flex-col">
+
+                  <h1>Profile</h1>
+
+                  <Card className="mb-6">
+                      <CardHeader>
+                        <CardDescription>
+                          Informações da sua conta
+                        </CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        <form onSubmit={handleSubmit}>                          
+                          <div className="space-y-2">
+                              <label htmlFor="name" className="text-sm font-medium">Nome</label>
+                              <Input 
+                                  id="name" 
+                                  type="text" 
+                                  placeholder="Nome" 
+                                  value={name} 
+                                  onChange={(e) => setName(e.target.value)} 
+                              />
+                          </div>
+                          <div className="space-y-2">
+                              <label htmlFor="email" className="text-sm font-medium">Email</label>
+                              <Input 
+                                  id="email" 
+                                  type="text" 
+                                  placeholder="Email" 
+                                  value={email} 
+                                  onChange={(e) => setEmail(e.target.value)} 
+                              />
+                          </div>
+                          <div className="space-y-2">
+                              <label htmlFor="password" className="text-sm font-medium">Password</label>
+                              <Input 
+                                  id="password" 
+                                  type="text" 
+                                  placeholder="Password" 
+                                  value={password} 
+                                  onChange={(e) => setPassword(e.target.value)} 
+                              />
+                          </div>
+                         
+                          
+                          <div className="flex gap-4 mt-4">
+                              <Button type="submit" className="flex-1  text-white cursor-pointer hover:bg-gray-800" variant="default" disabled={loading}>
+                                  {loading ? "Salvando..." : "Salvar"}
+                              </Button>
+                             
+                          </div>
+                      </form>
+                      </CardContent>
+                    </Card>
+                </main>
+            </SidebarProvider>
     </div>
   );
 }
